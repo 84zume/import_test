6107a445a1108b839f29e644808a5663a01dc82c
uchida <root@localhost.localdomain>
2016-08-30 15:12:45 +0900

    ローカル用のweb_consoleをapplication.rb→development.rbにに移動

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  
+  #ローカル用のweb_console
+  config.web_console.whitelisted_ips = '10.0.3.2'
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
